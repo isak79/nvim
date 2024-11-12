f8f1264c87b2f98b69f980f1795e71f09f6022c2
isak79 <isak.hansen74@hotmail.no>
2024-11-12 11:20:25 +0100

    removed unused plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 require("isak.core")
 require("isak.lazy")
+
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
--- lua/isak/plugins/vim-zellij.lua
+++
@@ -1,7 +0,0 @@
--- Lazy.nvim
-return {
-  "hiasr/vim-zellij-navigator.nvim",
-  config = function()
-    require("vim-zellij-navigator").setup()
-  end,
-}
